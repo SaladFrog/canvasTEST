87ef445c0e3ed57f709e3c01a7d8b41e1ecc7e01
SaladFrog <48181364+SaladFrog@users.noreply.github.com>
2021-01-06 10:54:12 +0100

    Create testES6.js

---
+++ testES6.js
@@ -0,0 +1,41 @@
+/*
+ *
+ */
+
+const settings = {
+  lorem: true,
+  dimensions: [500, 500]
+};
+
+const test = () => {
+  return ({ context, dimensions }) => {
+    context.fillStyle = "green";
+    context.fillRect(20, 10, 150, 100);
+  };
+};
+
+function createCanvas(test, settings = {}) {
+  const canvas = document.getElementById("canvas");
+  const context = canvas.getContext("2d");
+  [canvas.width, canvas.height] = settings.dimensions;
+
+  const dimensions = settings.dimensions;
+
+  const result = test(context, dimensions);
+  if (!result)
+    throw new TypeError("drawing function must return a valid object");
+
+  let drawing;
+  drawing = result;
+  setup();
+
+  function setup() {
+    draw();
+  }
+
+  function draw(dt = 0) {
+    drawing({ context, dimensions });
+  }
+}
+
+createCanvas(test, settings);
